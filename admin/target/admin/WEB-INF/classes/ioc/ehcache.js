1e64affc882df09691770268ab2ecf83de4b91d5
GuoTianChi <sygtch@163.com>
2019-10-25 10:56:43 +0800

    v1.0.0修改

--- admin/target/admin/WEB-INF/classes/ioc/ehcache.js
+++
@@ -1,7 +0,0 @@
-var ioc = {
-    "cacheManager": {
-        "type": "net.sf.ehcache.CacheManager",
-        "factory": "net.sf.ehcache.CacheManager#getCacheManager",
-        "args": ["admin"] // 对应shiro.ini中指定的ehcache.xml中定义的name
-    }
-};
